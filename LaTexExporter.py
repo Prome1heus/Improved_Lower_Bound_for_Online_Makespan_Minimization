c7915d16e66acaf6c71e028decee6247b34cbc87
Prome1heus <ginpa2000@gmail.com>
2021-07-12 14:49:06 +0200

    Visualisation of layers

--- LaTexExporter.py
+++ LaTexExporter.py
@@ -39,26 +39,26 @@ def write_overview(f, rounds: [Round], m: int, final_m: int, c: float):
             "\\cdot OPT(\\sigma)$. The job sequence consists of several rounds. We assume that m is a multiple of "
             + str(m) + ". \\newline\n")
 
-    for round in rounds:
-        f.write(round.get_overview())
-
     map_size_to_round = {0.0: 0}
     for round in rounds[:-1]:
         for subround in round.sub_rounds:
             map_size_to_round[float(subround.job_size)] = round.index
 
-    for round in rounds[:-1]:
-        result =  "\\begin{figure}[!htbp]\n"
-        result += "\\centering"
-        result += "\\includegraphics[scale = 0.35]{overview_" + str(round.index) + ".png}\n"
-        result += "\\caption{Overview of used job sizes in Round "+ str(round.index) + "}\n"
-        result += "\\end{figure}\n"
-        result += "\\FloatBarrier\n"
-        jobs = []
-        for subround in round.sub_rounds:
-            for _ in range(subround.multiplicity):
-                jobs.append([subround.job_size])
-        SchedulePlotter.plot_schedule(jobs, "overview_" + str(round.index), m, map_size_to_round=map_size_to_round)
+    for round in rounds:
+        f.write(round.get_overview())
+        if round != rounds[-1]:
+            result =  "\\begin{figure}[!htbp]\n"
+            result += "\\centering"
+            result += "\\includegraphics[scale = 0.35]{overview_" + str(round.index) + ".png}\n"
+            result += "\\caption{Overview of used job sizes in Round " + str(round.index) + "}\n"
+            result += "\\end{figure}\n"
+            result += "\\FloatBarrier\n"
+            f.write(result)
+            jobs = []
+            for subround in round.sub_rounds:
+                for _ in range(subround.multiplicity):
+                    jobs.append([subround.job_size])
+            SchedulePlotter.plot_schedule(jobs, "overview_" + str(round.index), m, map_size_to_round=map_size_to_round)
 
 
 def write_analysis(f, rounds: [Round]):
