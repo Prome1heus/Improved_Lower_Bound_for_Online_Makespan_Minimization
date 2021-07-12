309cc2c02f9a40819c61765465229082acd1f927
Prome1heus <ginpa2000@gmail.com>
2021-07-12 16:02:34 +0200

    Removed obsolete need to input final_m

--- main.py
+++ main.py
@@ -22,10 +22,9 @@ def handle_next_command(job_size: Fraction):
         for i in range(0, len(jobs_so_far), m):
             cutoff_value += jobs_so_far[i]
         cutoff_value += job_size
-
         cutoff_value = cutoff_value / c
 
-        print(cutoff_value)
+        print("Current maximum makespan allowed: " + str(float(cutoff_value)))
 
         sub_round = solver.solve(jobs_so_far, cutoff_value, job_size, multiplicity, False, greedy_ratio)
 
@@ -41,32 +40,23 @@ def handle_next_command(job_size: Fraction):
 
 def handle_finish():
     if len(jobs_so_far) % m != 0:
-        print("previous round inccomplete, exiting")
+        print("previous round incomplete, exiting")
         exit(1)
 
     job_size = Fraction(input('Enter the last job\n'))
-    last_sub_round = None
+    jobs_so_far.append(job_size)
+
     cutoff_value = 0
     for i in range(0, len(jobs_so_far), m):
         cutoff_value += jobs_so_far[i]
-    cutoff_value += job_size
     cutoff_value = cutoff_value / c
-    while last_sub_round is None:
-        for round in rounds:
-            for sub_round in round.sub_rounds:
-                print(float(sub_round.job_size), sub_round.multiplicity)
-        final_m = int(input('Enter the number of machines for last subround\n'))
-        if final_m % m != 0:
-            print('The number of machines must be a multiple of m!\n')
-            continue
-        print('trying with ' + str(final_m) + "jobs")
-        final_jobs = []
-        for job in jobs_so_far:
-            for _ in range(int(final_m / m)):
-                final_jobs.append(job)
-        final_jobs.append(job_size)
-        last_sub_round = solver.solve(final_jobs, cutoff_value, job_size, 1, True, final_greedy_ratio)
-    final_m = m
+
+    for round in rounds:
+        for sub_round in round.sub_rounds:
+            print(float(sub_round.job_size), sub_round.multiplicity)
+
+    last_sub_round = solver.solve(jobs_so_far, cutoff_value, job_size, 1, True, final_greedy_ratio)
+    final_m = last_sub_round.m
     if last_sub_round is not None:
         rounds[len(rounds) - 1].add_sub_round(last_sub_round)
 
@@ -119,8 +109,8 @@ if __name__ == '__main__':
     index = 2
 
     while True:
-        next_input = Fraction(input('Enter new job size\n'))
-        if next_input == 0:
+        next_input = input('Enter new job size\n')
+        if next_input == "finish":
             handle_finish()
             break
-        handle_next_command(next_input)
+        handle_next_command(Fraction(next_input))
